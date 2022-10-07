f50c3118b9dfead76818f4734f96c6c82a04e011
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-10-07 23:03:16 +0530

    removed nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Nav from './components/Nav';
+// import Nav from './components/Nav';
 import Hero from './components/Hero';
 import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <>
-    <Nav />
+    {/* <Nav /> */}
     <div id='LinksAbout'>
       <Routes>
         <Route path='/New-portfolio' element={<Hero />} exact />
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import './Nav.css';
-import { Link } from 'react-router-dom';
+// import React from 'react'
+// import './Nav.css';
+// import { Link } from 'react-router-dom';
 
 
-const Nav = () => {
-  return (
-    <>
-    <div className='NavbarLink'>
-        <li>
-          <Link to="/About">About</Link>
-        </li>
-        <li>Projects</li>
-        <li>Achievements</li>
-        <li>Open Source</li>
-    </div>
-    </>
-  )
-}
+// const Nav = () => {
+//   return (
+//     <>
+//     <div className='NavbarLink'>
+//         <li>
+//           <Link to="/About">About</Link>
+//         </li>
+//         <li>Projects</li>
+//         <li>Achievements</li>
+//         <li>Open Source</li>
+//     </div>
+//     </>
+//   )
+// }
 
 
-export default Nav
+// export default Nav
