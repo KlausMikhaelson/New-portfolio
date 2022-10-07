3604b9c206e893a9c67fd75d241491deb85a587f
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-10-08 00:38:25 +0530

    added progressbar in lang

--- client/src/components/Language.js
+++ client/src/components/Language.js
@@ -3,15 +3,22 @@ import Reactimg from '../Assets/react.png'
 import Nodejs from "../Assets/nodejs.png"
 import Js from "../Assets/js.png"
 import "./Language.css"
+import Java from "../Assets/java.png"
 
 
 const Language = () => {
   return (
     <>
       <div className='lang'>
+        <div className='react-lang'>
           <img src={Reactimg} alt="React" />
+          <div className='progress'>
+        <span className='react'></span>
+          </div>
+        </div>
           <img src={Nodejs} alt="NodeJs" />
           <img src={Js} alt="Js" />
+          <img src={Java} alt="Java" />
       </div>
     </>
   )
